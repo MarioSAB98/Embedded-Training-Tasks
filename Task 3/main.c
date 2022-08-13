021850520b77428a33c4d9afd02d853f8ef798df
MarioSAB98 <68597876+MarioSAB98@users.noreply.github.com>
2022-08-13 21:49:42 +0200

    edited task 3

--- Task 3/main.c
+++ Task 3/main.c
@@ -6,14 +6,15 @@ int main() {
     int exit = 1;
     int size = 0;
     int new = 0;
-    int i = 0,j = 0,temp = 0;
+    int i = 0, j = 0, temp = 0, search = 0;
     while (exit == 1)
     {
         printf("to add element to array press: 1\n");
         printf("to sort array in ascending order press: 2\n");
         printf("to sort array in descending order press: 3\n");
         printf("to print array press: 4\n");
-        printf("to exit press: 5\n");
+        printf("to search in array press: 5\n");
+        printf("to exit press: 6\n");
 
         scanf("%d", &choice);
         switch (choice)
@@ -56,14 +57,39 @@ int main() {
                 break;
 
             case 4:
-                printf("array : \n");
+                if (size > 0)
+                {
+                    printf("array : \n");
 
-                for (i = 0; i < size; ++i)
-                    printf("%d\n", array[i]);
-                printf("\n");
+                    for (i = 0; i < size; ++i)
+                        printf("%d\n", array[i]);
+                    printf("\n");
+                }
+                else
+                    printf("array is empty\n");
                 break;
 
             case 5:
+                printf("enter element to search array for\n");
+                scanf("%d", &temp);
+                for (i = 0; i < size; ++i)
+                {
+                    if(array[i] == temp)
+                    {
+                        printf("element found at index %d\n", i);
+                        search++;
+                    }
+                }
+
+                if(search == 0)
+                    printf("element was not found in the array\n");
+                else
+                    printf("element was found %d times in the array\n", search);
+
+                search = 0;
+                break;
+
+            case 6:
                 exit = 0;
                 break;
 
